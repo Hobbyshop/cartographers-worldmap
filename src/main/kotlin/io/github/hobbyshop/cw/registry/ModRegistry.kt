ee2215f7e903a7c62d396e1743897b8e0717839b
marc. <marc.deike@outlook.de>
2024-09-20 23:50:28 +0200

    cartography ink

--- src/main/kotlin/io/github/hobbyshop/cw/registry/ModRegistry.kt
+++ src/main/kotlin/io/github/hobbyshop/cw/registry/ModRegistry.kt
@@ -7,6 +7,7 @@ import net.minecraft.item.ItemStack
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.text.Text
+import net.minecraft.util.Rarity
 
 object ModRegistry {
 
@@ -16,17 +17,20 @@ object ModRegistry {
         .entries { _, entries ->
             entries.add(CARTOGRAPHERS_PEN)
             entries.add(WORLDMAP)
+            entries.add(CARTOGRAPHY_INK)
         }
         .build()
 
-    private val CARTOGRAPHERS_PEN = Item(Item.Settings())
+    private val CARTOGRAPHERS_PEN = Item(Item.Settings().rarity(Rarity.UNCOMMON))
     private val WORLDMAP = Item(Item.Settings().maxCount(1))
+    private val CARTOGRAPHY_INK = Item(Item.Settings())
 
     fun createRegistry() {
         Registry.register(Registries.ITEM_GROUP, id("group"), ITEM_GROUP)
 
         Registry.register(Registries.ITEM, id("cartographers_pen"), CARTOGRAPHERS_PEN)
         Registry.register(Registries.ITEM, id("worldmap"), WORLDMAP)
+        Registry.register(Registries.ITEM, id("cartography_ink"), CARTOGRAPHY_INK)
     }
 
 }
